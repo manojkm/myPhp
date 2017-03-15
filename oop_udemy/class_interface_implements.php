fdd1708912818c459797b0cb6b4a2467c6e8c0d0
Manoj <tvmmanu@gmail.com>
2017-03-15 16:10:58 +0530

    learned interface class

--- oop_udemy/class_interface_implements.php
+++ oop_udemy/class_interface_implements.php
@@ -1,36 +1,4 @@
 <?php
-
-class Cars {
-
-var $wheels =4;
-
-
-    public function greetings(){
-       echo  "This car has " .$this ->wheels . " Wheels";
-    }
-}
-
-
-class Truck extends Cars{
-
-    var $wheels =6;
-
-}
-
-$bmw = new Cars();
-$bmw->greetings();
-
-echo "<hr/>";
-
-$mer = new Truck();
-$mer->greetings();
-
-?>
-
-<?php
-
-echo "<hr/>";
-
 interface test{
     public function A($i);
     public function B($j = 20);
